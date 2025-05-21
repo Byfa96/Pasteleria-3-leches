ae64facb04b87b3a9d8c47885dcdb7183e85367e
Fabian <fab.parraguez@duocuc.cl>
2025-05-20 20:54:47 -0400

    Se agregaron lineas para servir archivos estaticos durante el desarrollo

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,7 @@ STATICFILES_DIRS = [join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media files solo para desarrollo
+MEDIA_URL = '/media/'
+MEDIA_ROOT = join(BASE_DIR, 'media')
